ce10ac18be80c5ef0bc90053e6252d62c02a4499
Kasper Nurminen <kasper@taiste.fi>
2020-01-04 15:21:37 +0200

    Added initialization script

---
+++ gameservice/management/commands/initialize.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User, Group
+from decimal import *
+from gameservice.models import Game, Payment, Score, Category
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        # superuser
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin').save()
+
+        # käyttäjät
+        testikayttaja = User(username="player", password="pass")
+        testidevaaja = User(username="developer", password="pass")
+        group = Group.objects.get(name='developers')   
+        testidevaaja.save()
+        testidevaaja.groups.add(group)
+        testikayttaja.save()
+        testidevaaja.save()
+        #kategoriat
+        rpg = Category(name="RPG")
+        singleplayer = Category(name="Singleplayer")
+        rpg.save()
+        singleplayer.save()
+
+        #pelit
+        testipeli = Game(title="Testipeli", developer=testidevaaja, price=Decimal(15.5), url="http://localhost:8001")
+        testipeli.save()
+        testipeli.categories.add(rpg)
+        testipeli.categories.add(singleplayer)
+        testipeli.save()
+
+        testipeli2 = Game(title="Testipeli2", developer=testidevaaja, price=Decimal(25.5), url="https://users.aalto.fi/~oseppala/game/example_game.html")
+        testipeli2.save()
+        testipeli2.categories.add(rpg)
+        testipeli2.save()
+
+        #score
+        score1 = Score(player=testikayttaja, score=100, game=testipeli)
+        score2 = Score(player=testikayttaja, score=200, game=testipeli)
+        score1.save()
+        score2.save()
+
+        #payment
+
+        payment = Payment(user=testikayttaja, price=testipeli.price, game=testipeli, pid="123", sid="123")
+        payment.save()
