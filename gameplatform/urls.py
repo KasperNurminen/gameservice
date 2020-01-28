56598617877e5af1e91ef733e016d37877fec8b4
Kasper Nurminen <kasper.nurminen@vincit.fi>
2020-01-28 13:10:55 +0200

    Added api

--- gameplatform/urls.py
+++ gameplatform/urls.py
@@ -16,24 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from gameservice.views import Main, GameDetail, Register, Developer, DeveloperEdit, DeveloperCreate, DeveloperDelete, DeveloperDetails, Purchase, PaymentSuccess, PaymentFailed, ScoreView, SaveDataView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("django.contrib.auth.urls")),
-    path('', Main.as_view(), name="main"),
-    path('game/<int:id>', GameDetail.as_view(), name="game"),
-    path('game/<int:id>/score', ScoreView.as_view(), name="score"),
-    path('game/<int:id>/save', SaveDataView.as_view(), name="save"),
-    path('register/', Register.as_view(), name="register"),
-    path('developer/<int:pk>/edit', DeveloperEdit.as_view(), name="developer-edit"),
-    path('developer/', Developer.as_view(), name='developer'),
-    path('developer/<int:pk>', DeveloperDetails.as_view(),
-         name="developer-details"),
-    path('developer/new', DeveloperCreate.as_view(), name="developer-create"),
-    path('developer/delete/<int:pk>',
-         DeveloperDelete.as_view(), name="developer-delete"),
-    path('purchase/<int:pk>', Purchase.as_view(), name="purchaseconfirmation"),
-    path('paymentSuccess', PaymentSuccess.as_view(), name="paymentSuccess"),
-    path('paymentFailed', PaymentFailed.as_view(), name="paymentFailed")
+    path('', include('gameservice.urls')),
+    path('api/', include('api.urls')),
 ]
---
+++ gameservice/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from gameservice.views import Main, GameDetail, Register, Developer, DeveloperEdit, DeveloperCreate, DeveloperDelete, DeveloperDetails, Purchase, PaymentSuccess, PaymentFailed
+
+urlpatterns = [
+    path('', Main.as_view(), name="main"),
+    path('game/<int:id>', GameDetail.as_view(), name="game"),
+    path('register/', Register.as_view(), name="register"),
+    path('developer/<int:pk>/edit', DeveloperEdit.as_view(), name="developer-edit"),
+    path('developer/', Developer.as_view(), name='developer'),
+    path('developer/<int:pk>', DeveloperDetails.as_view(),
+         name="developer-details"),
+    path('developer/new', DeveloperCreate.as_view(), name="developer-create"),
+    path('developer/<int:pk>/delete',
+         DeveloperDelete.as_view(), name="developer-delete"),
+    path('purchase/<int:pk>', Purchase.as_view(), name="purchaseconfirmation"),
+    path('paymentSuccess', PaymentSuccess.as_view(), name="paymentSuccess"),
+    path('paymentFailed', PaymentFailed.as_view(), name="paymentFailed")
+]
