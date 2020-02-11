9a63243d8f736ea4617a93eb8e97342a6038328a
Kasper Nurminen <kasper.nurminen@vincit.fi>
2020-02-11 13:03:56 +0200

    Updated initialization script

--- gameservice/management/commands/initialize.py
+++ gameservice/management/commands/initialize.py
@@ -7,36 +7,40 @@ from gameservice.models import Game, Payment, Score, Category
 class Command(BaseCommand):
     def handle(self, **options):
         # superuser
-        User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+        User.objects.create_superuser('admin', 'admin@example.com', 'adm1n1')
 
         # käyttäjät
         testikayttaja = User.objects.create_user(
-            'player', 'player@example.com', 'pass')
+            'player', 'player@example.com', 'ple1jeri')
         testidevaaja = User.objects.create_user(
-            'developer', 'developer@example.com', 'pass')
+            'developer', 'developer@example.com', 'develoupper1')
+        otto = User.objects.create_user(
+            'otto', 'otto@example.com', 'develoupper2')
         group = Group.objects.get(name='developers')
         testidevaaja.save()
         testidevaaja.groups.add(group)
+        otto.groups.add(group)
+        otto.save()
         testikayttaja.save()
         testidevaaja.save()
         # kategoriat
-        rpg = Category(name="RPG")
-        singleplayer = Category(name="Singleplayer")
-        rpg.save()
-        singleplayer.save()
+        rpg = Category(name="RPG").save()
+        action = Category(name="Action").save()
+        sport = Category(name="Sport").save()
+        puzzle = Category(name="Puzzle").save()
 
         # pelit
-        testipeli = Game(title="Testipeli", developer=testidevaaja,
-                         price=Decimal(15.5), url="/static/games/game2.html")
+        testipeli = Game(title="Oma peli", developer=testidevaaja,
+                         price=Decimal(15.5), url="../static/games/game2.html")
         testipeli.save()
-        testipeli.categories.add(rpg)
-        testipeli.categories.add(singleplayer)
+        testipeli.categories.add(puzzle)
         testipeli.save()
 
-        testipeli2 = Game(title="Testipeli2", developer=testidevaaja, price=Decimal(
+        testipeli2 = Game(title="Oton peli", developer=otto, price=Decimal(
             25.5), url="https://users.aalto.fi/~oseppala/game/example_game.html")
         testipeli2.save()
         testipeli2.categories.add(rpg)
+        testipeli2.categories.add(action)
         testipeli2.save()
 
         # score
@@ -47,6 +51,6 @@ class Command(BaseCommand):
 
         # payment
 
-        payment = Payment(user=testikayttaja, price=testipeli.price,
+        payment = Payment(user=developer, price=testipeli.price,
                           game=testipeli, pid="123", sid="123")
         payment.save()
